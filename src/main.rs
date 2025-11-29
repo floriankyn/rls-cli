8ceeab099afbdb0cbd642fb91bf7ffd204e551a4
Florian Lepage <florian.kyn@gmail.com>
2025-11-29 20:00:50 +0100

    add basic logic for ls cmd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{ fs, env };
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let current_dir = env::current_dir().unwrap();
+    let contents = fs::read_dir(current_dir).unwrap();
+
+    for item in contents {
+        let item = item.unwrap().path();
+
+        let item_filename = Path::new(item.file_name().unwrap()).display();
+
+        match item.is_dir() {
+            true => println!("{}", item_filename),
+            false => println!("{}", item_filename)
+        }
+    }
 }
